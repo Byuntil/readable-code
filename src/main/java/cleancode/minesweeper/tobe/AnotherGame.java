80c8f22808fb457dc398c4ec8081812057888207
Byuntil <gugugu2014@jbnu.ac.kr>
2024-08-15 17:27:38 +0900

    ISP 리팩토링

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,15 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.game.GameRunnable;
+
+public class AnotherGame implements GameRunnable {
+//    @Override
+//    public void initialize() {
+//        //필요없는데..-> Game 이라는 인터페이스가 2가지 기능을 가지고 있구나 -> 분리하자
+//    }
+
+    @Override
+    public void run() {
+        //게임 진행
+    }
+}
